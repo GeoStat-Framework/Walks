12d5f46637c66a2ca12c0caaeac2c4fedaf203bb
LSchueler <lennart.schueler@ufz.de>
2019-06-14 20:48:42 +0200

    Added the possibility to add particle sources

--- walks/simulation.py
+++ walks/simulation.py
@@ -27,6 +27,13 @@ OUTPUT = {
 }
 
 
+class Sources(object):
+    def __init__(self):
+        self.t = []
+        self.pos = []
+        # TODO use getters to automatically increase idx
+        self.idx = 0
+
 class Simulation(object):
     """Perform particle experiments.
 
@@ -64,6 +71,10 @@ class Simulation(object):
         # will be initialised by self.initial_condition()
         self.pos = None
         self.N = None
+        self.sources = Sources()
+        # add one timepoint after max. simulation time for the pops to not through
+        # an exception when all sources have been added
+        self.sources.t = [self.T + self.dt]
 
         if output in OUTPUT:
             out = OUTPUT[output]
@@ -89,6 +100,17 @@ class Simulation(object):
         self.pos = np.repeat(pos, distribution, axis=1)
         self.N = self.pos.shape[1]
 
+    def add_sources(self, times, pos, distribution=1):
+        self.sources.t = list(times)
+        # add one timepoint after max. simulation time for the pops to not through
+        # an exception when all sources have been added
+        self.sources.t.append([self.T+self.dt])
+        self.sources.pos = np.array(pos)
+        distribution = np.atleast_1d(distribution)
+        if len(distribution) == 1:
+            distribution = np.repeat(distribution, len(times))
+        self.sources.distribution = list(distribution)
+
     def __call__(self, seed=None):
         """Simulate the random walk.
         
@@ -101,16 +123,32 @@ class Simulation(object):
             raise RuntimeError('Set the initial conditions before simulating.')
 
         rngs = self._create_rng_streams(self.dim, seed)
-        jumps = np.empty_like(self.pos)
+        self.jumps = np.empty_like(self.pos)
+
+        print('Starting simulation with {} walkers.'.format(self.N))
 
         # write initial conditions to file
         self.output.write_timestep(0., self.pos)
         for timestep, t in enumerate(np.arange(0., self.T, self.dt)):
-            jumps[:,:] = [rngs[d].standard_normal(self.N) for d in range(self.dim)]
+            self.jumps[:,:] = [rngs[d].standard_normal(self.N) for d in range(self.dim)]
             drift = self.field(self.pos, **self.field_kwargs)
-            euler_maruyama(self.pos, drift, jumps, self.D, self.dt)
+            euler_maruyama(self.pos, drift, self.jumps, self.D, self.dt)
+            if t <= self.sources.t[self.sources.idx] < t + self.dt:
+                self._apply_sources()
             if timestep % self.nsave == 0:
                 self.output.write_timestep(t, self.pos)
+        print('Simulation ended with {} walkers.'.format(self.N))
+
+    def _apply_sources(self):
+        source_pos = self.sources.pos[:,self.sources.idx]
+        if len(source_pos.shape) == 1:
+            source_pos = source_pos[:,np.newaxis]
+        distribution = self.sources.distribution[self.sources.idx]
+        source_pos = np.repeat(source_pos, distribution, axis=1)
+        self.pos = np.hstack((self.pos, source_pos))
+        self.N = self.pos.shape[1]
+        self.jumps = np.empty_like(self.pos)
+        self.sources.idx += 1
 
     def _create_rng_streams(self, dim, seed):
         """Create a RNG stream for each spatial dimension.
